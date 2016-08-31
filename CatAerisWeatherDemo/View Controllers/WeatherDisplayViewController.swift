464acaf049561cac59adaf18b950296915023991
Louis Tur <louis.tur@gmail.com>
2016-08-31 15:17:06 -0400

    adds location service keys/manager & tests

--- CatAerisWeatherDemo/View Controllers/WeatherDisplayViewController.swift
+++ CatAerisWeatherDemo/View Controllers/WeatherDisplayViewController.swift
@@ -7,29 +7,61 @@
 //
 
 import UIKit
+import SnapKit
+import Aeris
 
-class WeatherDisplayViewController: UIViewController {
-
-    override func viewDidLoad() {
-        super.viewDidLoad()
-        self.view.backgroundColor = UIColor.blueColor()
-        // Do any additional setup after loading the view.
-    }
-
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
+class WeatherDisplayViewController: UIViewController, CLLocationManagerDelegate {
+  
+  internal var currentPlace: AWFPlace!
+  internal var observationLoader: AWFObservationsLoader!
+  
+  internal var locationManager: CLLocationManager = CLLocationManager()
+  
+  override func viewDidLoad() {
+    super.viewDidLoad()
+    self.view.backgroundColor = UIColor.blueColor()
+    self.locationManager.delegate = self
+    
+    self.checkLocationAuthorization()
+  }
+  
+  override func didReceiveMemoryWarning() {
+    super.didReceiveMemoryWarning()
+    // Dispose of any resources that can be recreated.
+  }
+  
+  func checkLocationAuthorization() {
+    if CLLocationManager.authorizationStatus() == .NotDetermined {
+      self.locationManager.requestAlwaysAuthorization()
     }
     
-
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepareForSegue(segue: UIStoryboardSegue, sender: AnyObject?) {
-        // Get the new view controller using segue.destinationViewController.
-        // Pass the selected object to the new view controller.
+  }
+  
+  
+  func locationManager(manager: CLLocationManager, didFailWithError error: NSError) {
+    print("location service error")
+  }
+  
+  func locationManagerDidResumeLocationUpdates(manager: CLLocationManager) {
+    print("did resume location updates")
+  }
+  
+  func locationManager(manager: CLLocationManager, didStartMonitoringForRegion region: CLRegion) {
+    print("did start monitoring for region")
+  }
+  
+  func locationManager(manager: CLLocationManager, didChangeAuthorizationStatus status: CLAuthorizationStatus) {
+    print("did change auth status")
+    
+    if status == .AuthorizedAlways || status == .AuthorizedWhenInUse {
+      self.locationManager.startUpdatingLocation()
     }
-    */
-
+    else if status == .NotDetermined {
+      print("status undetermined")
+    }
+  }
+  
+  func locationManager(manager: CLLocationManager, didUpdateLocations locations: [CLLocation]) {
+    print("locations updated")
+  }
 }
